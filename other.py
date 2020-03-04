cd3f535ff094cb59a58c34443656da02730d682e
xottabb14 <eduardyo@mail.ru>
2020-03-04 17:31:37 +0200

    Update other.py

--- other.py
+++ other.py
@@ -27,6 +27,18 @@ def blynk_send(blynk_token,blynk_pin,blynk_value):
 	url_get = "http://%s/%s/update/%s?value=%s" % (blynk_serv,blynk_token,blynk_pin,blynk_value)
 	response = requests.request("GET", url_get)
 
+#дергаем вебхук blynk RGB
+def blynk_sendRGB(blynk_token,blynk_pin,blynk_red,blynk_green,blynk_blue):
+	url_get = "http://%s/%s/update/%s?value=%s&value=%s&value=%s" % (blynk_serv,blynk_token,blynk_pin,blynk_red,blynk_green,blynk_blue)
+	response = requests.request("GET", url_get)
+
+#дергаем вебхук any с параметром RGB
+def any_val_sendRGB(url_any,red,green,blue):
+	url_any = url_any.replace("--vr--",red)
+	url_any = url_any.replace("--vg--",green)
+	url_any = url_any.replace("--vb--",blue)
+	response = requests.request("GET", url_any)
+
 #вебхуки GET
 #any get all (получаем значение после запроса - строковое)
 def any_get_all(url_any):
@@ -86,6 +98,9 @@ def other_dev_main(dev_param):
 					capabil_params = capabil_params+'{"type":"%srange","retrievable":true,"parameters":{"instance":%s,"random_access":true,"range":{"max":%s,"min":%s,"precision":%s}}},' % (llen,instance_str,max_val,min_val,prec)
 				else:
 					pass
+		#умение для RGB
+		elif "color_setting" in str(capab_list[0]):
+			capabil_params = capabil_params+'{"type":"%scolor_setting","retrievable":true,"parameters":{"color_model":"rgb"}},' % (llen)
 		else:
 			pass
 	
@@ -94,6 +109,9 @@ def other_dev_main(dev_param):
 	
 	all_str = first_part_str+capabil_params+third_path_str
 	return all_str
+
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
 
 #функция обработки статуса умений
 def status_other(dev_param,dev_id,req_save):
@@ -190,6 +208,38 @@ def status_other(dev_param,dev_id,req_save):
 
 				val_rang = 100
 				capabil_params = capabil_params+'{"type":"%srange","state":{"instance":%s,"value":%s}},' % (llen,instance_str,val_rang)
+		#статус RGB
+		elif "color_setting" in str(capab_list[0]):
+			if "blynk" in str(capab_list[4].replace("\"","")).lower():
+				status_io = str(capab_list[7].replace("\"","")).lower() #статус или нет
+			elif "any" in str(capab_list[4].replace("\"","")).lower():
+				status_io = str(capab_list[6].replace("\"","")).lower()
+			else:
+				status_io = "nostatus"
+			if "status" in status_io:
+				if "ifttt" in str(capab_list[4].replace("\"","")).lower():
+						pass #не поддерживается
+				if "blynk" in str(capab_list[4].replace("\"","")).lower():
+					blynk_token = str(capab_list[6]).replace("\"","")
+					blynk_pin = str(capab_list[5]).replace("\"","")
+					current_val = blynk_get_val(blynk_token,blynk_pin)
+					list_rgb = current_val.split(",")#разбиваем текущую строку на параметры
+
+					red = int(round((255/(int(capab_list[1].replace("\"",""))))*int(list_rgb[0])))
+					green = int(round((255/(int(capab_list[2].replace("\"",""))))*int(list_rgb[1])))
+					blue = int(round((255/(int(capab_list[3].replace("\"",""))))*int(list_rgb[2])))
+					print ("RGB_from_blynk ",str(red)," ",str(green)," ",str(blue))
+					rgb_all = rgb_to_hex((red, green, blue))
+					print (str(rgb_all))
+
+					val_rgb = int(rgb_all,16)
+					print ("\nR "+str(red)+" G "+str(green)+" B "+str(blue))
+					print ("\ndec = ",val_rgb)
+					capabil_params = capabil_params+'{"type":"%scolor_setting","state":{"instance":"rgb","value":%s}},' % (llen,val_rgb)
+				if "any" in str(capab_list[4].replace("\"","")).lower():
+					capabil_params = capabil_params+'{"type":"%scolor_setting","state":{"instance":"rgb","value":0}},' % (llen)
+			else:
+				capabil_params = capabil_params+'{"type":"%scolor_setting","state":{"instance":"rgb","value":0}},' % (llen)
 		else:
 			pass
 	capabil_params = capabil_params[0:(len(capabil_params)-1)]
@@ -253,7 +303,7 @@ def control_other(dev_id,req_save,dev_param,num_dev):
 				pass		
 		all_str = '{"id":"%s","capabilities":[{"type":"devices.capabilities.on_off","state":{"instance":"%s","action_result": {"status": "DONE"}}}]},' % (dev_id,intence_cur)
 		
-	if "devices.capabilities.toggle" in type_capability:
+	elif "devices.capabilities.toggle" in type_capability:
 		for capabil in capabilities: #для каждой строки в файле
 			capabil_list = functions.clean_text(str(capabil)).split(",")
 			test_intance = str(capabil_list[1])
@@ -274,7 +324,7 @@ def control_other(dev_id,req_save,dev_param,num_dev):
 					pass
 		all_str = '{"id":"%s","capabilities":[{"type":"devices.capabilities.toggle","state":{"instance":"%s","action_result": {"status": "DONE"}}}]},' % (dev_id,intence_cur)
 
-	if "devices.capabilities.range" in type_capability:
+	elif "devices.capabilities.range" in type_capability:
 		#текущее значение интенса. Например - 50.
 		resp_val = str(req_save['payload']['devices'][num_dev]['capabilities'][0]['state']['value'])
 		for capabil in capabilities:#для каждой строки в файле
@@ -298,6 +348,65 @@ def control_other(dev_id,req_save,dev_param,num_dev):
 				else:
 					pass
 		all_str = '{"id":"%s","capabilities":[{"type":"devices.capabilities.range","state":{"instance":"%s","action_result": {"status": "DONE"}}}]},' % (dev_id,intence_cur)
+	elif "devices.capabilities.color_setting" in type_capability: #если в запросе есть color_setting
+		resp_val = str(req_save['payload']['devices'][num_dev]['capabilities'][0]['state']['value']) #принятое значение RGB
+		for capabil in capabilities:#для каждой строки в файле other.txt
+			capabil_list = functions.clean_text (str(capabil)).split(",")#чистим от энтеров и делим параметры на список
+			test_capabil = "devices.capabilities."+str(capabil_list[0]) #задаем строку из файла для поиска
+			test_capabil = test_capabil.replace("\"","")#строка из файла для сравнения с полученным типом умения
+
+			if type_capability in test_capabil: #если тип умнеия есть в текущей строке файла
+				if "ifttt" in str(capabil_list[4].replace("\"","")).lower(): #если ifttt
+					print ("\nIFTTT color_setting_not")
+				elif "blynk" in str(capabil_list[4].replace("\"","")).lower(): #если blynk
+					print ("\nBLYNK color_setting")
+					blynk_token = str(capabil_list[6]).replace("\"","")
+					blynk_pin = str(capabil_list[5]).replace("\"","")
+					print("\nЗначение RGB_управление = ",str(resp_val))
+					hex_val = "%06x" % int(resp_val)
+					print ("hex_val = ",hex_val)
+					str_rgb = str(tuple(int(hex_val[i:i+2], 16) for i in (0, 2, 4)))
+					print ("str_rgb = ",str_rgb)
+					str_rgb_lst = (str_rgb[1:(len(str_rgb)-1)]).replace(" ","")
+					str_rgb_lst = str_rgb_lst.split(",")
+					red_r = int(capabil_list[1].replace("\"",""))
+					green_r = int(capabil_list[2].replace("\"",""))
+					blue_b = int(capabil_list[3].replace("\"",""))
+					red_cur = int(str_rgb_lst[0])
+					green_cur = int(str_rgb_lst[1])
+					blue_cur = int(str_rgb_lst[2])
+					red = str(round((red_r/255)*red_cur))
+					green = str(round((green_r/255)*green_cur))
+					blue = str(round((blue_b/255)*blue_cur))
+					print("\nHEX RGB_управление = ",red," ",green," ",blue)
+					blynk_sendRGB(blynk_token,blynk_pin,red,green,blue)
+					
+				elif "any" in str(capabil_list[4].replace("\"","")).lower(): #если другое устройство
+					url_any = str(capabil_list[5].replace("\"",""))
+					print ("\nANY color_setting")
+					print("\nЗначение RGB_управление = ",str(resp_val))
+					hex_val = "%06x" % int(resp_val)
+					print ("hex_val = ",hex_val)
+					str_rgb = str(tuple(int(hex_val[i:i+2], 16) for i in (0, 2, 4)))
+					print ("str_rgb = ",str_rgb)
+					str_rgb_lst = (str_rgb[1:(len(str_rgb)-1)]).replace(" ","")
+					str_rgb_lst = str_rgb_lst.split(",")
+					red_r = int(capabil_list[1].replace("\"",""))
+					green_r = int(capabil_list[2].replace("\"",""))
+					blue_b = int(capabil_list[3].replace("\"",""))
+					red_cur = int(str_rgb_lst[0])
+					green_cur = int(str_rgb_lst[1])
+					blue_cur = int(str_rgb_lst[2])
+					red = str(round((red_r/255)*red_cur))
+					green = str(round((green_r/255)*green_cur))
+					blue = str(round((blue_b/255)*blue_cur))
+					print("\nHEX RGB_управление = ",red," ",green," ",blue)
+					any_val_sendRGB(url_any,red,green,blue)
+				else:
+					print ("\nnoconnect color_setting")
+			else:
+				print ("\nNO color_setting")
+		all_str = '{"id":"%s","capabilities":[{"type":"devices.capabilities.color_setting","state":{"instance":"rgb","action_result": {"status": "DONE"}}}]},' % (dev_id)
 	else:
 		pass
 	return all_str
