c57635d58f2521439b7352f406cadaa022652a86
xottabb14 <eduardyo@mail.ru>
2020-03-05 15:16:08 +0200

    Update other.py

--- other.py
+++ other.py
@@ -180,8 +180,12 @@ def status_other(dev_param,dev_id,req_save):
 			instance_str = str(capab_list[1])
 			if "blynk" in str(capab_list[5].replace("\"","")).lower():
 				status_io = str(capab_list[8].replace("\"","")).lower() #статус или нет
+				max_in_dev = int(capab_list[9].replace("\"","")) #максимальный предел на устройстве
+
 			else:
 				status_io = str(capab_list[7].replace("\"","")).lower() #статус или нет
+				max_in_dev = int(capab_list[11].replace("\"","")) #максимальный предел на устройстве
+			max_yandex = int(capab_list[3].replace("\"","")) #максимальный предел в УДЯ
 
 			if "status" in status_io: #если статус
 				if "ifttt" in str(capab_list[5].replace("\"","")).lower():
@@ -190,7 +194,7 @@ def status_other(dev_param,dev_id,req_save):
 					blynk_token = str(capab_list[7]).replace("\"","")
 					blynk_pin = str(capab_list[6]).replace("\"","")
 					current_val = blynk_get_val(blynk_token,blynk_pin)
-
+					current_val = int(round((max_yandex/max_in_dev)*int(current_val)))
 					capabil_params = capabil_params+'{"type":"%srange","state":{"instance":%s,"value":%s}},' % (llen,instance_str,current_val)
 				elif "any" in str(capab_list[5].replace("\"","")).lower():
 
@@ -225,8 +229,8 @@ def status_other(dev_param,dev_id,req_save):
 					current_val = blynk_get_val(blynk_token,blynk_pin)
 					list_rgb = current_val.split(",")#разбиваем текущую строку на параметры
 
-					red = int(round((255/(int(capab_list[1].replace("\"",""))))*int(list_rgb[0])))
-					green = int(round((255/(int(capab_list[2].replace("\"",""))))*int(list_rgb[1])))
+					green = int(round((255/(int(capab_list[1].replace("\"",""))))*int(list_rgb[0])))
+					red = int(round((255/(int(capab_list[2].replace("\"",""))))*int(list_rgb[1])))
 					blue = int(round((255/(int(capab_list[3].replace("\"",""))))*int(list_rgb[2])))
 					print ("RGB_from_blynk ",str(red)," ",str(green)," ",str(blue))
 					rgb_all = rgb_to_hex((red, green, blue))
@@ -335,15 +339,20 @@ def control_other(dev_id,req_save,dev_param,num_dev):
 				if "ifttt" in str(capabil_list[5].replace("\"","")).lower():
 					pass
 				elif "blynk" in str(capabil_list[5].replace("\"","")).lower():
+					max_in_dev = int(capabil_list[9].replace("\"","")) #максимальный предел на устройстве
+					max_yandex = int(capabil_list[3].replace("\"","")) #максимальный предел в УДЯ
 					blynk_token = str(capabil_list[7]).replace("\"","")
 					blynk_pin = str(capabil_list[6]).replace("\"","")
 					on_val = str(resp_val)
+					on_val = str(round((max_in_dev/max_yandex)*int(on_val)))
 
 					blynk_send(blynk_token,blynk_pin,on_val)
 				elif "any" in str(capabil_list[5].replace("\"","")).lower():
+					max_in_dev = int(capabil_list[8].replace("\"","")) #максимальный предел на устройстве
+					max_yandex = int(capabil_list[3].replace("\"","")) #максимальный предел в УДЯ
 					url_any_on = str(capabil_list[6]).replace("\"","")
 					value_any = str(resp_val)
-
+					value_any = str(round((max_in_dev/max_yandex)*int(value_any)))
 					any_val_send(url_any_on,value_any)
 				else:
 					pass
@@ -375,8 +384,8 @@ def control_other(dev_id,req_save,dev_param,num_dev):
 					red_cur = int(str_rgb_lst[0])
 					green_cur = int(str_rgb_lst[1])
 					blue_cur = int(str_rgb_lst[2])
-					red = str(round((red_r/255)*red_cur))
-					green = str(round((green_r/255)*green_cur))
+					green = str(round((red_r/255)*red_cur))
+					red = str(round((green_r/255)*green_cur))
 					blue = str(round((blue_b/255)*blue_cur))
 					print("\nHEX RGB_управление = ",red," ",green," ",blue)
 					blynk_sendRGB(blynk_token,blynk_pin,red,green,blue)
